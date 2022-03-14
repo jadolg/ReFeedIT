248fcb4af3d5581a25fc0266f501c28761930814
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2022-03-14 17:59:29 +0100

    Add defaults for entries

--- refeeds/feed_parser.py
+++ refeeds/feed_parser.py
@@ -8,14 +8,17 @@ from refeeds.models import Feed, Entry, get_entry_hash
 def parse_feed(feed: Feed):
     feed_reader = feedparser.parse(feed.url)
     for entry in feed_reader.get('entries'):
-        if not Entry.objects.filter(hash=get_entry_hash(entry.title, entry.description, entry.link)):
+        entry_hash = get_entry_hash(entry.get("title", "???"),
+                                    entry.get("description", "???"),
+                                    entry.get("link", "???"))
+        if not Entry.objects.filter(hash=entry_hash):
             logging.info(f"saving entry {entry}")
             new_entry = Entry(
                 feed=feed,
-                title=entry.title,
-                description=entry.description,
-                link=entry.link,
-                hash=get_entry_hash(entry.title, entry.description, entry.link)
+                title=entry.get("title", "???"),
+                description=entry.get("description", "???"),
+                link=entry.get("link", "???"),
+                hash=entry_hash
             )
             new_entry.save()
         else:
